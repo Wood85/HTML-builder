f3918ed56b3a20adc5bd62d3cb9b0cb3b4b0381d
Aleksandr <megalistnn@gmail.com>
2024-01-20 17:35:20 +0300

    feat: implement task 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const fs = require('fs');
+const { stdin, stdout, exit } = process;
+const pathToRecords = path.join(__dirname, 'text.txt');
+let body = '';
+
+function init() {
+  fs.writeFile(pathToRecords, body, (error) => {
+    if (error) return console.error(error.message);
+  });
+}
+
+fs.access(pathToRecords, fs.constants.F_OK, (err) => {
+  if (err) {
+    init();
+  } else {
+    fs.readFile(pathToRecords, (error, data) => {
+      if (error) return console.error(error.message);
+      body = data;
+    });
+  }
+});
+
+stdout.write('Hello, enter something:\n');
+
+stdin.on('data', (data) => {
+  if (data.toString().split('').slice(0, -2).join('') === 'exit') {
+    exit();
+  }
+  body += data.toString();
+  init();
+  stdout.write('Enter something else:\n');
+});
+
+process.on('SIGINT', () => exit());
+process.on('exit', () => stdout.write('Goodbye friend!'));
