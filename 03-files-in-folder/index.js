1cbe639dfdffda8e61f8f3f53df52d58a2c4b310
Aleksandr <megalistnn@gmail.com>
2024-01-21 16:50:51 +0300

    feat: implement task 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+const pathToSecretDir = path.join(__dirname, 'secret-folder');
+fs.readdir(pathToSecretDir, { withFileTypes: true }, (err, files) => {
+  if (err) return console.error(err.message);
+  files.forEach((obj) => {
+    fs.stat(`${obj.path}\\${obj.name}`, (err, stats) => {
+      if (err) return console.error(err.message);
+      if (stats.isFile()) {
+        console.log(
+          `${obj.name.split('.')[0]}-${obj.name.split('.')[1]}-${
+            stats['size']
+          }`,
+        );
+      }
+    });
+  });
+});
